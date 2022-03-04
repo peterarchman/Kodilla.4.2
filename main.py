0e3ad210652870badc1b73d03e458a88478c1e48
Peter Archman <archman.peter@gmail.com>
2022-03-04 11:43:17 +0100

    Finish main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_palindrom(text):
-    lenght = len(text)
-    if text[0] == text[-1]:
+def is_palindron(text):
+    if text == text[-1::-1]:
         return True
     else:
         return False
 
-is_palindrom("kajak")
+is_palindron("potop")
